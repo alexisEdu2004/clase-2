8111583a906688b422c584edf66fd9ac8993f97a
alexisEdu2004 <max34junior@gmail.com>
2024-02-17 22:25:33 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hello-world';
+  name = 'Alexis Eduardo Aguilar Zacarias';
+  number = '20962';
+  text = '5A';
+
 }
